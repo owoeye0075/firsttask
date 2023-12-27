0a98c5e788cefaa72d20cad4ebe0ff3f050715a4
owoeye0075 <owoeye0075@gmail.com>
2023-12-27 02:44:56 +0000

    first task

---
+++ class.js
@@ -0,0 +1,41 @@
+let ranNumb = []
+// let age = 6
+
+function getLicense(age, bribe){
+
+    if(age<18 || bribe<300){
+
+        console.log(`you have passed`);
+    
+    }
+    else{
+        console.log("you have failed")
+    }
+}
+getLicense(15, 350);
+
+
+
+
+
+//For Loop example
+// for(i =0; i<10; i+=2){
+//     if(i===numb){
+//         break
+//     }
+
+//     ranNumb.push(i);
+   
+// }
+
+// }
+// addMyFriend(6)
+// // myfriend[3] =""
+// // addMyFriend("yinka");
+// // addMyFriend("John");
+// // addMyFriend("Ade");
+// // addMyFriend("Femi");
+
+// // myfriend[1] = "funmi";
+
+// console.log(`the Random number is ${ranNumb}`);
